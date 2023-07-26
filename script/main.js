e2d62b04cb4de20124e80b013acaaf1b44b18155
sandun7787 <sandunwarnasooreya345@gmail.com>
2023-07-26 20:10:31 +0530

    first commit

---
+++ script/main.js
@@ -0,0 +1,170 @@
+// let yname = prompt('enter your name','saman');
+// console.log(yname)
+
+// let yname1 =confirm('are you amila');
+// console.log(yname)
+
+// let a ={
+//     name:'amila',
+//     age:4,
+//     city:'kandy',
+// }
+
+// let b={
+//     name:'sandun',
+//     age:5,
+//     city:'kandy',
+// }
+
+// let c ={
+//     name:'pakaya',
+//     age:10,
+//     city:'kandy',
+
+// }
+
+// console.table({a,b,c});
+
+let A ='SANDUN WARANASOORIYA';
+let b=23;
+let c= true;
+let d = 234n;
+let e;
+let f = null;
+let a = -4;
+console.log(2);
+
+b=String(b);
+c= String(c);
+
+console.log(A,b,c,d)
+
+
+var yname='sandunwarnasooreya';
+
+
+let yname='sandun warnasooreya';
+
+const yname='sandunwaranasooreya';;;
+
+
+let g =20;
+let v=45;
+let h=56;
+console.log('')
+
+let l,q,x = 20;
+
+let k =['amila',12,12,12,12,true[1,2,3,4,5]];
+console.log(d.reverse())
+
+let m="sandun waranasooreya".split();
+
+A.unshift('sadlo')
+A.push(false)
+
+A.shift();
+
+a[1] = "sandun";
+console.log(a)
+
+a=b=c=20;
+
+let a ={
+    name:"sandun",
+    age:13,
+    city:'kandy',
+    language:[java,javaScript,c,html],
+
+    city:{
+        address1:'hoidi',
+        addres2:'paka'
+    },
+    city:'thangalkla'
+
+    
+};
+console.log(a.language[3].city[address1])
+
+A.class ="java fulll couser"
+
+function name(name,age,clz=12){
+  let name= prompt('Enter your name');
+  console.log(`my name ${name} and my age ${age} and your class is ${clz}`)
+
+  return age+name;
+}
+
+name("sandun",23,'js');
+name("pakaya",45),'php';
+name("huththi",89,'hh');
+
+let sandun =(name,age,clz)=>{
+
+    console.log(`my name is ${name} and my age${age}`)
+
+    return (name+clz)
+}
+
+
+
+
+sandun("sandun1",23,'js');
+sandun("pakaya",23,'ll');
+sandun("huththa",56,'ffs');
+
+
+
+let firstName =name1=>`your name is${name1}`;
+
+
+
+firstName('sandunwarnasooreya')
+
+
+let age=23;
+let sandun34 =(paka)=>{
+    let age =20;
+    return `your name is${age}`;
+
+}
+
+sandun34('huththa')
+console.log(age);
+
+
+let a = 5,b=6;
+console.log(s>b);
+
+console.log(a<b);
+console.log(a>=b);
+console.log(a <=b);
+console.log();
+console.log();
+console.log();
+console.log();
+console.log();
+
+
+console.log((2>4)&&(5<6));
+
+let a = 34;
+if(a>75){
+    console.log("pass");
+
+
+}else if(marks>65){
+    console.log("falls");
+
+}else if(marks>55){
+    console.log(n);
+}else if(marks>55){
+    console.log(s);
+
+}else{
+    console.log(marks>35);
+}
+
+
+(mark>50)?console.log('pass') :(mark>65)?console.log('B'):(mark>55)
+
